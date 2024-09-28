a525e193fc6f90f1d16c9fcb18ce4dcb2b85c8f8
KrishBarad <krish.barad018@gmail.com>
2024-09-28 05:05:29 -0500

    Add files via upload

---
+++ assessment/Assessment 1.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+main() 
+{
+  int choice, num1, num2, result;
+
+  while (1) 
+  {
+    printf("-------------------MENU-------------------\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("5. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    switch (choice) 
+	{
+      case 1:
+        result = num1 + num2;
+        printf("Result: %d\n", result);
+        break;
+      case 2:
+        result = num1 - num2;
+        printf("Result: %d\n", result);
+        break;
+      case 3:
+        result = num1 * num2;
+        printf("Result: %d\n", result);
+        break;
+      case 4:
+          result = num1 / num2;
+          printf("Result: %d\n", result);
+        break;
+      default:
+        printf("Invalid choice\n");
+    
+    }
+  }
+
+}
